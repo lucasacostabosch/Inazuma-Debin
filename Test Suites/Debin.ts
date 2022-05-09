74f98d0e386cd1ef990ba99b129c4775255b0f2e
Jorge Nieto <JNieto@coelsa.com.ar>
2022-05-09 11:40:20 -0300

    Cambios en Debin

---
+++ Test Suites/Debin.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Debin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e1545d1d-9c72-478d-8b1b-b30e65da0198</testSuiteGuid>
+   <testCaseLink>
+      <guid>e334f497-7170-4ac8-9349-55319c0df1e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1.- Orquestador</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>fefa74bc-c998-49c9-80e4-a3acb3630a0c</id>
+         <iterationEntity>
+            <iterationType>SPECIFIC</iterationType>
+            <value>4</value>
+         </iterationEntity>
+         <testDataId>Data Files/Debin</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>fefa74bc-c998-49c9-80e4-a3acb3630a0c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>IDCasoDePrueba</value>
+         <variableId>82db791e-d9c2-4efa-a81b-db12417b11c2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>fefa74bc-c998-49c9-80e4-a3acb3630a0c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Acciones</value>
+         <variableId>371d56fb-ccbc-4c89-99c3-300b681419cb</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>fefa74bc-c998-49c9-80e4-a3acb3630a0c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Parametros</value>
+         <variableId>11549d36-c968-456f-977d-8d5e6ff88523</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>fefa74bc-c998-49c9-80e4-a3acb3630a0c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Respuestas</value>
+         <variableId>3c04e80b-5d55-4a09-a21d-64f0ec41e584</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>fefa74bc-c998-49c9-80e4-a3acb3630a0c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>ObjetivoPrueba</value>
+         <variableId>6e7299df-4c6d-4c27-a54d-6caf0310d98d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
