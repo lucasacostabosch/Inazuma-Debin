0fc12cf0e4abf1413c04b4d6e77758c4aaef7ae3
Milagros Atencio <Milagros Atencio@DESKTOP-OUUEE3M>
2022-05-19 14:22:11 -0300

    Automatizacion Credin

--- Test Suites/Credin.ts
+++ Test Suites/Credin.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>QR</name>
+   <name>Credin</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,48 +10,48 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>34fe862d-17ec-47aa-90a0-c0485e238a05</testSuiteGuid>
+   <testSuiteGuid>965fdae3-ecf4-45db-b7f0-a51d28ff4c07</testSuiteGuid>
    <testCaseLink>
-      <guid>016c9088-ffc3-4d56-8a7a-890924693825</guid>
+      <guid>8942886a-3706-4498-b569-ef48798a7b43</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/1.- Orquestador</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>0ad17699-ac49-45c9-890c-1b820170aca6</id>
+         <id>4ec48e0a-978b-419b-a639-39f546d904f8</id>
          <iterationEntity>
             <iterationType>ALL</iterationType>
-            <value>3</value>
+            <value>48</value>
          </iterationEntity>
-         <testDataId>Data Files/QR</testDataId>
+         <testDataId>Data Files/Credin</testDataId>
       </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId>0ad17699-ac49-45c9-890c-1b820170aca6</testDataLinkId>
+         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>IDCasoDePrueba</value>
          <variableId>82db791e-d9c2-4efa-a81b-db12417b11c2</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>0ad17699-ac49-45c9-890c-1b820170aca6</testDataLinkId>
+         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Acciones</value>
          <variableId>371d56fb-ccbc-4c89-99c3-300b681419cb</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>0ad17699-ac49-45c9-890c-1b820170aca6</testDataLinkId>
+         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Parametros</value>
          <variableId>11549d36-c968-456f-977d-8d5e6ff88523</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>0ad17699-ac49-45c9-890c-1b820170aca6</testDataLinkId>
+         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Respuestas</value>
          <variableId>3c04e80b-5d55-4a09-a21d-64f0ec41e584</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>0ad17699-ac49-45c9-890c-1b820170aca6</testDataLinkId>
+         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>ObjetivoPrueba</value>
          <variableId>6e7299df-4c6d-4c27-a54d-6caf0310d98d</variableId>
