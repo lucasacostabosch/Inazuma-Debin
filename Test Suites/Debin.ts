8a28d43f99bfaa3367622aee4df1032b2c215406
unknown <matencio.externo@coelsa.com.ar>
2022-07-25 12:28:59 -0300

    sprint 39 automatizado

--- Test Suites/Debin.ts
+++ Test Suites/Debin.ts
@@ -20,8 +20,8 @@
          <combinationType>ONE</combinationType>
          <id>fefa74bc-c998-49c9-80e4-a3acb3630a0c</id>
          <iterationEntity>
-            <iterationType>ALL</iterationType>
-            <value>3, 5</value>
+            <iterationType>RANGE</iterationType>
+            <value>15-19</value>
          </iterationEntity>
          <testDataId>Data Files/Debin</testDataId>
       </testDataLink>
