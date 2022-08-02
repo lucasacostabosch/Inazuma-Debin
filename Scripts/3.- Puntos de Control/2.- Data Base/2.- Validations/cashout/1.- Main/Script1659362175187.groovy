489f4d9b859d1774078c8aa3b3d3f1f7a0b5f2c1
Jorge Nieto <JNieto@coelsa.com.ar>
2022-08-02 11:17:18 -0300

    Validacion de tipo de contracargo y contracargoqr

--- Scripts/3.- Puntos de Control/2.- Data Base/2.- Validations/cashout/1.- Main/Script1659362175187.groovy
+++ Scripts/3.- Puntos de Control/2.- Data Base/2.- Validations/cashout/1.- Main/Script1659362175187.groovy
@@ -17,3 +17,19 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+String  Accion = GlobalVariable.Accion
+
+Map dato = [db:[:],errores:[]]
+
+'Validar datos de body contra DDBB'
+Map dato1 = WebUI.callTestCase(findTestCase('3.- Puntos de Control/2.- Data Base/2.- Validations/'+ Accion + '/2.- Validate Body'), 
+    [('response') : response], FailureHandling.STOP_ON_FAILURE)
+
+'Validar datos del request contra DDBB'
+Map dato2 = WebUI.callTestCase(findTestCase('3.- Puntos de Control/2.- Data Base/2.- Validations/'+ Accion + '/3.- Validate Response'),
+	[('response') : response], FailureHandling.STOP_ON_FAILURE)
+
+dato.db = dato1.db + dato2.db
+dato.errores = dato1.errores + dato2.errores
+
+return dato
--- Scripts/3.- Puntos de Control/2.- Data Base/2.- Validations/cashout/3.- Validate Response/Script1659362203448.groovy
+++ Scripts/3.- Puntos de Control/2.- Data Base/2.- Validations/cashout/3.- Validate Response/Script1659362203448.groovy
@@ -1,19 +1,92 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import groovy.json.JsonSlurper
+import com.kms.katalon.core.util.KeywordUtil
+import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+def jsonSlurper = new JsonSlurper()
+
+def Body = GlobalVariable.Body
+def Accion = GlobalVariable.Accion
+Map respuesta
+
+if (response != null) {
+		
+	def id_hash = response.id
+	
+	Map select = CustomKeywords.'sql.DML.select'('DEBIN', '*, DATEDIFF (Minute, DAC_ADD_DT, DAC_FECHA_EXPIRACION) as TIEMPOEXPIRACION', 'DEBIN_ACTIVAS', 'DAC_ID_HASH =\''+id_hash+'\'', '')[0]
+	
+	if (select != null) {
+	
+		String tipo, id, fecha_negocio
+				
+		if(select.get('DAC_TIPO') == 'CONTRACARGOQR') {
+			tipo	= 	'contraCargo'
+		}else {
+			tipo 	= 	select.get('DAC_TIPO').toLowerCase()
+		}
+		
+		id	= 	select.get('DAC_ID_HASH')
+		
+		if (response.fecha_negocio == '0001-01-01T00:00:00') {
+			fecha_negocio 	=	'0001-01-01' 		
+		}else {
+			fecha_negocio 	=	select.get('DAC_FECHA_NEGOCIO') 	
+		}
+
+		Map dato_db = [:]
+		
+		dato_db.tipo			= 	tipo
+		dato_db.id				= 	id
+		dato_db.fecha_negocio	=	fecha_negocio	
+		
+		String tipoB, idB, fecha_negocioB1
+		
+		tipoB 						= 	response.tipo
+		idB 						= 	response.id
+		fecha_negocioB1 			= 	response.fecha_negocio
+		String[] fecha_negocioB2	= 	fecha_negocioB1.split("T")
+		String fecha_negocioB		= 	fecha_negocioB2[0]	
+				
+		Map response1 = [:]
+		
+		response1.tipo				=	tipoB
+		response1.id				= 	idB
+		response1.fecha_negocio 	= 	fecha_negocioB
+		
+		errores = coelsa.Util.validar(response1, dato_db)
+		
+		respuesta = [
+				db:[
+					querybody:	"SELECT * FROM DEBIN_ACTIVAS WHERE DAC_ID_HASH =\'$response.id\'",
+					selectresponse: select
+					],
+					errores: errores
+				]
+	}else {
+		errores = 'Response: Consulta sin resultados. '
+		respuesta = [
+						db: [
+							querybody:	"SELECT * FROM DEBIN_ACTIVAS WHERE DAC_ID_HASH =\'$response.id\'",
+							selectbody:	select
+							],
+						errores: errores
+					]
+	}
+		
+}else {
+	errores = 'Respuesta vacia. '
+	db = ''
+	respuesta = [
+					db:db,
+					errores: errores
+				]
+}
+
+return respuesta
+
 
