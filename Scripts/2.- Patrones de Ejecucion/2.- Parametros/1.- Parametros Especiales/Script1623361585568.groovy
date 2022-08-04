b523a1af89991d67d74ec5936bf221cca1a545d2
ocardoso <ocardoso@OCARDOS-CQRYLW2.dominio.local>
2022-08-04 12:21:29 -0300

    Fix Parametros Especiales

--- Scripts/2.- Patrones de Ejecucion/2.- Parametros/1.- Parametros Especiales/Script1623361585568.groovy
+++ Scripts/2.- Patrones de Ejecucion/2.- Parametros/1.- Parametros Especiales/Script1623361585568.groovy
@@ -16,11 +16,12 @@ if(Parametros.objeto != null) {
 if(Parametros.ori_trx_id != null)
 	Parametros.ori_trx_id = GlobalVariable.Debin.debin_trx
 
-if(Parametros.sql != null)
+if(Parametros.sql != null) {
 	if(Parametros.sql.toString().toLowerCase().equals("off")) {
 		GlobalVariable.Debin.sql = false
 	} else {
 		GlobalVariable.Debin.sql = true
 	}
+}
 	
 return Parametros
