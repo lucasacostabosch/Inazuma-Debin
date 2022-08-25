c041cf40cdb78969ec7c7d8013a7ee92c429bb22
matencio <matencio.externo@coelsa.com.ar>
2022-08-25 13:20:04 -0300

    credito forzado2

--- Test Suites/QR.ts
+++ Test Suites/QR.ts
@@ -20,8 +20,8 @@
          <combinationType>ONE</combinationType>
          <id>fefa74bc-c998-49c9-80e4-a3acb3630a0c</id>
          <iterationEntity>
-            <iterationType>SPECIFIC</iterationType>
-            <value>13</value>
+            <iterationType>RANGE</iterationType>
+            <value>1-12</value>
          </iterationEntity>
          <testDataId>Data Files/QR</testDataId>
       </testDataLink>
