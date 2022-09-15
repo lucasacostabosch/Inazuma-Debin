fa6208221320ad58927e574a5f31a355538d6df3
unknown <matencio.externo@coelsa.com.ar>
2022-09-15 17:40:16 -0300

    algunos cambios en los TC de credito Forzado

--- Test Suites/Credito Forzado.ts
+++ Test Suites/Credito Forzado.ts
@@ -20,8 +20,8 @@
          <combinationType>ONE</combinationType>
          <id>5ec57477-61b0-4d6b-b0eb-32605305f210</id>
          <iterationEntity>
-            <iterationType>RANGE</iterationType>
-            <value>48-49</value>
+            <iterationType>ALL</iterationType>
+            <value>60</value>
          </iterationEntity>
          <testDataId>Data Files/Credito Forzado</testDataId>
       </testDataLink>
