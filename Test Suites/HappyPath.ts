bbc3266bb794c29f721c05c2f249b55feaf12ba0
unknown <matencio.externo@coelsa.com.ar>
2022-09-27 11:13:32 -0300

    TCs Happy Path

--- Test Suites/HappyPath.ts
+++ Test Suites/HappyPath.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Credin</name>
+   <name>HappyPath</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,48 +10,48 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>965fdae3-ecf4-45db-b7f0-a51d28ff4c07</testSuiteGuid>
+   <testSuiteGuid>5dbf8fb0-8129-4af0-88f8-8e6f7518d02b</testSuiteGuid>
    <testCaseLink>
-      <guid>8942886a-3706-4498-b569-ef48798a7b43</guid>
+      <guid>70ad60b3-736c-4af6-829b-81a6da34177d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/1.- Orquestador</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>4ec48e0a-978b-419b-a639-39f546d904f8</id>
+         <id>d3561815-d588-4633-a062-d5199f435527</id>
          <iterationEntity>
-            <iterationType>SPECIFIC</iterationType>
-            <value>7</value>
+            <iterationType>ALL</iterationType>
+            <value></value>
          </iterationEntity>
-         <testDataId>Data Files/Credin</testDataId>
+         <testDataId>Data Files/HappyPath</testDataId>
       </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
+         <testDataLinkId>d3561815-d588-4633-a062-d5199f435527</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>IDCasoDePrueba</value>
          <variableId>82db791e-d9c2-4efa-a81b-db12417b11c2</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
+         <testDataLinkId>d3561815-d588-4633-a062-d5199f435527</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Acciones</value>
          <variableId>371d56fb-ccbc-4c89-99c3-300b681419cb</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
+         <testDataLinkId>d3561815-d588-4633-a062-d5199f435527</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Parametros</value>
          <variableId>11549d36-c968-456f-977d-8d5e6ff88523</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
+         <testDataLinkId>d3561815-d588-4633-a062-d5199f435527</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Respuestas</value>
          <variableId>3c04e80b-5d55-4a09-a21d-64f0ec41e584</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>4ec48e0a-978b-419b-a639-39f546d904f8</testDataLinkId>
+         <testDataLinkId>d3561815-d588-4633-a062-d5199f435527</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>ObjetivoPrueba</value>
          <variableId>6e7299df-4c6d-4c27-a54d-6caf0310d98d</variableId>
